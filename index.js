361ec91fd2f4ccc258f3c9021fc80f6f387da8c5
servicerecup38-creator <service.recup38@gmail.com>
2026-05-08 03:02:29 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '8593461177:AAHmikhJtAed4e_S-pk4sNbAng5f-8p0J6s';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id,
+`🔥 Bienvenue sur TECH.REWARDS
+
+⚡ Boutique premium
+💎 Récompenses exclusives
+🛡 Support 24/7
+
+👇 Ouvre la boutique ci-dessous`,
+{
+reply_markup: {
+inline_keyboard: [
+[
+{
+text: '🛒 OUVRIR LA BOUTIQUE',
+url: 'https://servicerecup38-creator.github.io'
+}
+],
+[
+{
+text: '💬 SUPPORT',
+url: 'https://t.me/contactgrenoble_cityfarms'
+}
+]
+]
+}
+});
+});
